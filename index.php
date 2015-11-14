d9603fd0eb48f9b217e30e59113e36b30bf3096f
Daniel Hewes <daniel@danimalweb.co.uk>
2015-11-13 18:03:40 -0800

    Add inset theme data function. WIP.

--- index.php
+++ index.php
@@ -48,7 +48,44 @@ $valid = function ($request, $response, $next) {
   return $response;
 };
 
+function insert_theme_data($data) {
+
+  // @TODO: Get rid of this and update the lambda db class to look for the env file automagically
+  $database_connection_settings = array(
+    'dbms'     => 'mysql',
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'database' => getenv('DB_NAME'),
+    'host'     => getenv('DB_HOST')
+  );
+
+  $conn = Lambda\Database\Connector::connect($database_connection_settings);
+  $db   = Lambda\Database\Connector::getInstance();
+
+  $sql  = "INSERT INTO theme_data ( theme_name, theme_version, theme_author, site_name, site_url, ip_address, wordpress_version )
+           VALUES ( ?, ?, ?, ?, ?, ?, ? )"; //Build the SQL.
+
+  $stmt = $db->prepare($sql); //Prep the SQL.
+
+  $stmt->bind_param('sssssss', $data['theme_name'], $data['theme_version'], $data['theme_author'], $data['site_name'], $data['site_url'], $data['ip_address'], $data['wordpress_version']); //Bind Params
+
+  $result = $stmt->execute(); //Execute!
+
+  if ($result) { // Ok
+    return true;
+  } else {
+    $response->withStatus(400); // Bad request
+    $response->withHeader('Content-type', 'application/json'); // Override existing header with new header.
+    $response->write(json_encode(array(
+      'error' => true,
+      'message' => 'Failed to insert data.'
+    )));
+    return $response;
+  }
+}
+
 $app->post('/api/theme', function ($request, $response, $args) {
+  $result = insert_theme_data($request->getParsedBody());
 
   $response->write(json_encode(array(
     'error' => false,
